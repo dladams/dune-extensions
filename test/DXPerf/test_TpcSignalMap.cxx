29adcba243e892be99a07407235103bc8a99d6af
David Adams <dladams@bnl.gov>
2015-09-04 14:08:17 -0400

    Finish TpcSignalMap.

--- test/DXPerf/test_TpcSignalMap.cxx
+++ test/DXPerf/test_TpcSignalMap.cxx
@@ -3,7 +3,7 @@
 // David Adams
 // May 2015
 //
-// Test script for Range.
+// Test script for TpcSignalMap.
 
 #include "DXPerf/TpcSignalMap.h"
 
@@ -24,6 +24,7 @@ int main() {
 #endif
   // Default map.
   string line = "-----------------------------";
+  int dbg = 0;
 
   cout << myname << line << endl;
   cout << myname << "Default signal map:" << endl;
@@ -35,20 +36,31 @@ int main() {
   assert(sm0.binCount() == 0);
 
   cout << myname << line << endl;
-  cout << myname << "Local fill of signal map:" << endl;
+  cout << myname << "Create and check geometry:" << endl;
   GeoHelper gh("dune35t4apa_v5", true);
-  gh.print();
+  if ( dbg ) gh.print();
   assert(gh.geometry() != nullptr);
+
+  cout << myname << line << endl;
+  cout << myname << "Local fill of signal map:" << endl;
   TpcSignalMap sm1("sm1", &gh, false);
-  sm1.setDbg(1);
+  sm1.setDbg(dbg);
   assert(sm1.check() == 0);
-  assert( sm1.addSignal(1500, 201, 41.0) == 0);
-  assert( sm1.addSignal(1500, 202, 81.0) == 0);
+  assert( sm1.addSignal(1500, 201, 4.1) == 0);
+  assert( sm1.addSignal(1500, 202, 8.1) == 0);
+  assert( sm1.addSignal(1500, 204, 7.1) == 0);
+  assert( sm1.addSignal(1500, 205, 7.1) == 0);
+  assert( sm1.addSignal(1501, 204, 2.1) == 0);
+  assert( sm1.addSignal(1501, 205, 6.1) == 0);
+  assert( sm1.addSignal(1501, 206, 9.1) == 0);
+  assert( sm1.addSignal(1501, 207, 3.1) == 0);
   sm1.print(cout, 3, myname);
-  assert(sm1.channelCount() == 1);
+  cout << myname << "Channel count: " << sm1.channelCount() << endl;
   cout << myname << "Tick count: " << sm1.tickCount() << endl;
-  assert(sm1.tickCount() == 1);
-  assert(sm1.binCount() == 1);
+  cout << myname << "Bin count: " << sm1.binCount() << endl;
+  assert(sm1.channelCount() == 2);
+  assert(sm1.tickCount() == 7);
+  assert(sm1.binCount() == 8);
 
   cout << myname << line << endl;
   cout << myname << "Ending test" << endl;
