07ab8ac0f05f1f31c4b2931f0fc083b639a321d3
David Adams <dladams@bnl.gov>
2015-09-14 12:55:57 -0400

    Add DXArt

---
+++ DXArt/ArtServiceHelper.cxx
@@ -0,0 +1,186 @@
+// ArtServiceHelper.cxx
+
+#include "DXArt/ArtServiceHelper.h"
+#include <iostream>
+#include <iomanip>
+#include <memory>
+#include "fhiclcpp/ParameterSet.h"
+#include "fhiclcpp/make_ParameterSet.h"
+#include "art/Framework/Services/Registry/ActivityRegistry.h"
+#include "art/Framework/Services/Registry/ServiceToken.h"
+#include "art/Framework/EventProcessor/ServiceDirector.h"
+#include "art/Framework/Services/System/TriggerNamesService.h"
+
+using std::string;
+using std::ostream;
+using std::cout;
+using std::endl;
+using std::setw;
+using std::find;
+using std::unique_ptr;
+using fhicl::ParameterSet;
+using fhicl::make_ParameterSet;
+using art::ServiceToken;
+using art::ServiceDirector;
+using art::ActivityRegistry;
+using art::ServiceRegistry;
+using art::TriggerNamesService;
+
+typedef ArtServiceHelper::NameList NameList;
+typedef ArtServiceHelper::ConfigurationMap ConfigurationMap;
+
+//**********************************************************************
+// Local helpers.
+//**********************************************************************
+
+namespace {
+
+// Return the art activity register.
+ActivityRegistry& ar() {
+  static unique_ptr<ActivityRegistry> par(new ActivityRegistry);
+  return *par.get();
+}
+
+}  // end unnamed namespace
+
+//**********************************************************************
+// Public members.
+//**********************************************************************
+
+ArtServiceHelper& ArtServiceHelper::instance() {
+  return *instancePtr().get();
+}
+  
+//**********************************************************************
+
+void ArtServiceHelper::close() {
+  delete instance().m_poperate;               // Close existing services and registry.
+  instance().m_poperate = nullptr;            // Reset the old operatre.
+  instancePtr().reset(new ArtServiceHelper);  // Delete the old service helper.
+  instance().m_load = 3;                      // Put new instance in deleted state.
+}
+  
+//**********************************************************************
+
+int ArtServiceHelper::addService(string name, string scfg) {
+  string myname = "ArtServiceHelper::addService: ";
+  if ( m_load == 1 || m_load == 2 ) {
+    cout << myname << "ERROR: Services may not be added after services are loaded." << endl;
+    return 1;
+  } else if ( m_load == 3 ) {
+    cout << myname << "ERROR: Services may not be added after service hlper is closed." << endl;
+    return 3;
+  } else if ( m_load != 0 ) {
+    cout << myname << "ERROR: Unexpected load status: " << m_load << "." << endl;
+    return 4;
+  }
+  if ( find(m_names.begin(), m_names.end(), name) != m_names.end() ) {
+    cout << myname << "ERROR: Service " << name << " is already registered" << endl;
+    return 5;
+  }
+  if ( name == "TriggerNamesService" ) {
+    cout << myname << "ERROR: TriggerNamesService is configured automatically." << endl;
+    return 6;
+  }
+  m_names.push_back(name);
+  m_cfgmap[name] = scfg;
+  if ( m_scfgs.size() ) m_scfgs += " ";
+  m_scfgs += scfg;
+  if ( name == "TFileService" ) m_needTriggerNamesService = true;
+  return 0;
+}
+
+//**********************************************************************
+
+int ArtServiceHelper::loadServices() {
+  string myname = "ArtServiceHelper::loadServices: ";
+  if ( m_load == 1 ) {
+    cout << myname << "ERROR: Services are already loaded." << endl;
+    return 1;
+  }
+  if ( m_load == 2 ) {
+    cout << myname << "ERROR: Service load failed earlier." << endl;
+    return 2;
+  }
+  if ( m_load == 3 ) {
+    cout << myname << "ERROR: Service manager is closed." << endl;
+    return 2;
+  }
+  // Register standard services.
+  ParameterSet cfgServices;
+  make_ParameterSet(fullServiceConfiguration(), cfgServices);
+  ServiceToken serviceToken;
+  ServiceDirector director(cfgServices, ar(), serviceToken);
+  // Register special services.
+  if ( m_needTriggerNamesService ) {
+    ParameterSet cfgTriggerNamesService;
+    string scfgTriggerNamesService = "process_name: \"myproc\"";
+    make_ParameterSet(scfgTriggerNamesService, cfgTriggerNamesService);
+    std::vector<string> tns;
+    unique_ptr<TriggerNamesService> ptns(new TriggerNamesService(cfgTriggerNamesService, tns));
+    director.addSystemService(std::move(ptns));
+  }
+  // Make the services available
+  // We need to keep this object around if we want to use the services.
+  //// m_poperate.reset(new ServiceRegistry::Operate(serviceToken));
+  m_poperate = new ServiceRegistry::Operate(serviceToken);
+  return m_load = 1;
+}
+
+//**********************************************************************
+
+NameList ArtServiceHelper::serviceNames() const {
+  return m_names;
+}
+
+//**********************************************************************
+
+string ArtServiceHelper::serviceConfiguration(std::string name) const {
+  ConfigurationMap::const_iterator icfg = m_cfgmap.find(name);
+  if ( icfg == m_cfgmap.end() ) return "";
+  return icfg->second;
+}
+
+//**********************************************************************
+
+string ArtServiceHelper::fullServiceConfiguration() const {
+  return m_scfgs;
+}
+
+//**********************************************************************
+
+int ArtServiceHelper::serviceStatus() const {
+  return m_load;
+}
+
+//**********************************************************************
+
+void ArtServiceHelper::print(ostream& out) const {
+  if ( m_load == 3 ) {
+    out << "Service manager is closed" << endl;
+    return;
+  }
+  out << "ArtServiceHelper has " << m_names.size() << " "
+      << (m_names.size()==1 ? "entry" : "entries")
+      << (m_names.size()==0 ? "" : ":") << endl;
+  unsigned int wnam = 8;
+  for ( string name : m_names ) if ( name.size() > wnam ) wnam = name.size();
+  for ( string name : m_names ) {
+    out << setw(wnam+2) << name << ":: " << m_cfgmap.find(name)->second << endl;
+  }
+  if ( m_load == 0 ) out << "Services have not been loaded." << endl;
+  else if ( m_load == 1 ) out << "Services have been loaded and are available." << endl;
+  else if ( m_load == 2 ) out << "Service load failed." << endl;
+  else out << "Unrecognized load status: " << m_load << "." << endl;
+}
+
+//**********************************************************************
+// Private members.
+//**********************************************************************
+
+std::unique_ptr<ArtServiceHelper>& ArtServiceHelper::instancePtr() {
+  static unique_ptr<ArtServiceHelper> psh(new ArtServiceHelper);
+  return psh;
+}
+  
+//**********************************************************************
---
+++ DXArt/ArtServiceHelper.h
@@ -0,0 +1,89 @@
+// ArtServiceHelper.h
+
+#ifndef ArtServiceHelper_H
+#define ArtServiceHelper_H
+
+// David Adams
+// September 2015
+//
+// This class loads art services outside the art framework.
+//
+// Information about art services can be found at:
+//   https://cdcvs.fnal.gov/redmine/projects/art/wiki/Guide_to_writing_and_using_services
+
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <memory>
+#include "art/Framework/Services/Registry/ServiceRegistry.h"
+
+class ArtServiceHelper {
+
+public:
+
+  typedef std::vector<std::string> NameList;
+  typedef std::map<std::string, std::string> ConfigurationMap;
+
+  // Return the one instance of this (singleton) class.
+  static ArtServiceHelper& instance();
+
+  // Delete the one instance of this class.
+  // Services are not longer available.
+  // The current instance of this class and all services are deleted.
+  static void close();
+
+  // Dtor.
+  ~ArtServiceHelper() = default;
+
+  // Add a service.
+  //   name - Name of the service, e.g. "TFileService"
+  //   scfg - Configuration string for the service, e.g. for TFileService:
+  //          TFileService: {service_type: "TFileService" fileName: "test.root"}
+  // Configuration format is the same as that found in the services block of an fcl file.
+  // Returns 0 for success.
+  int addService(std::string name, std::string scfg);
+
+  // Load the services, i.e. make them avialble for use via art::ServiceHandle.
+  // Returns the status: 1 for success, 2 for failure.
+  int loadServices();
+
+  // Return the names of added services.
+  NameList serviceNames() const;
+
+  // Return the configuration string for a service.
+  std::string serviceConfiguration(std::string name) const;
+
+  // Return the full configuration string.
+  std::string fullServiceConfiguration() const;
+
+  // Return the service status.
+  //   0 - not loaded
+  //   1 - services loaded and available
+  //   2 - service load failed
+  //   3 - service helper is closed
+  int serviceStatus() const;
+
+  // Display the contents and status of a service helper.
+  void print(std::ostream& out =std::cout) const;
+
+private:
+
+  // Return the pointer to the one instance of this (singleton) class.
+  static std::unique_ptr<ArtServiceHelper>& instancePtr();
+
+  // Ctors.
+  ArtServiceHelper() = default;
+  ArtServiceHelper(const ArtServiceHelper&) = delete;
+  ArtServiceHelper& operator=(const ArtServiceHelper&) const = delete;
+
+  NameList m_names;
+  ConfigurationMap m_cfgmap;
+  std::string m_scfgs;
+  int m_load = 0;
+  bool m_needTriggerNamesService = false;
+  art::ServiceRegistry::Operate* m_poperate;
+
+};
+
+#endif
