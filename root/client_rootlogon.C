1648d6f19ef5a9e881bc9d9502e616a48d6b5850
David Adams <dladams@bnl.gov>
2015-10-21 15:53:05 -0400

    Add Root scripts and env var to product.

--- root/client_rootlogon.C
+++ root/client_rootlogon.C
@@ -5,12 +5,13 @@
 // Relevant changes should be propagated back.
 
 {
-  cout << "Welcome to DXRoot." << endl;
+  int dbg = 1;
+  if ( dbg > 0 ) cout << "Welcome to DXRoot." << endl;
   string mpath = TROOT::GetMacroPath();
   mpath += "$DUNE_EXTENSIONS_ROOTSCRIPTS:";
-  cout << "Macro path: " << mpath << endl;
+  if ( dbg > 1 ) cout << "Macro path: " << mpath << endl;
   TROOT::SetMacroPath(mpath.c_str());
   gSystem->AddIncludePath("-I$DUNE_EXTENSIONS_ROOTSCRIPTS");
-  cout << "Include path: " << gSystem->GetIncludePath() << endl;
+  if ( dbg > 1 ) cout << "Include path: " << gSystem->GetIncludePath() << endl;
   gROOT->ProcessLine(".X $DUNE_EXTENSIONS_ROOTSCRIPTS/rootlogon.C");
 }
