da970bc42e1170fa380ceec76a58283a6153bb07
David Adams <dladams@bnl.gov>
2015-11-12 15:54:58 -0500

    Add HistoCompare.

---
+++ root/HistoCompare.h
@@ -0,0 +1,39 @@
+// HistoCompare.h
+//
+// David Adams
+// November 2015
+//
+// Class to compare histograms.
+
+#ifndef HistoCompare_H
+#define HistoCompare_H
+
+#include <string>
+#include "TFile.h"
+#include "TH1.h"
+
+class HistoCompare {
+
+public:
+
+  // Ctor from the two input files.
+  HistoCompare(std::string afname1, std::string afname2);
+
+  // Compare histogram with same name in the two files.
+  // Returns the number of differing bins (nbad).
+  int compare(std::string hname);
+
+  // Make comparison for all 35t planes: apa0u, ..., apa3z2
+  int compare35t(std::string hpre);
+
+  // Data.
+  std::string fname1;
+  std::string fname2;
+  int dbg;
+  int nbad;
+  int nbin;
+
+};
+
+#endif
+
