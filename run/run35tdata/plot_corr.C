815b797ffa467aa4b0d15103d9461f5f082c6bbc
David Adams <dladams@bnl.gov>
2016-03-09 13:27:26 -0500

    Fix dxdsplit to not copy input file.

--- run/run35tdata/plot_corr.C
+++ run/run35tdata/plot_corr.C
@@ -1,8 +1,10 @@
 void plot_corr(TH2* phin, float cmin, float cmax) {
   ostringstream ssnam;
   ssnam << phin->GetName() << "_" << cmin << "_" << cmax;
+  new TCanvas;
   TH2* ph = phin->Clone(ssnam.str().c_str());
   ph->GetXaxis()->SetRangeUser(cmin, cmax);
   ph->GetYaxis()->SetRangeUser(cmin, cmax);
   ph->Draw("colz");
+  addaxis();
 }
